651d7a1c9278d65dfd9a992522c28b5beef044d7
Puck-The-Bard <zdmcguire1@buffs.wtamu.edu>
2019-02-06 23:37:56 -0600

    Pizza app with some react

---
+++ src/js/Pg1.js
@@ -0,0 +1,13 @@
+var root = document.getElementById('pepparoni');
+var root = document.getElementById('sardine');
+var root = document.getElementById('supreme');
+
+var content = (
+
+    <button class = "ButtonColor" >Click Me!</button>
+
+)
+
+ReactDOM.render(content, pepparoni)
+ReactDOM.render(content, sardine)
+ReactDOM.render(content, supreme)
